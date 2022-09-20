e74d181c07df5cdcaeef34d8db2e97c803c6aa6d
Bilal Mülayim <bilalmulayim@Bilal-MBP.station>
2022-09-20 21:30:35 +0300

    added seventh course

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,26 @@
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Home Page</h2>
-        </div>
-     );
-}
- 
+  const handleClick = (e) => {
+    console.log("hello ninjas", e);
+  };
+
+  const handleClickAgain = (name , e) => {
+    console.log("hello ninjas" + name);
+  };
+
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={(e) => handleClickAgain("mario" , e)}
+      >
+        Click me again
+      </button>
+    </div>
+  );
+};
+
 export default Home;
 <div className="home">
-    <h2>Home Page</h2>
-</div>
+  <h2>Home Page</h2>
+</div>;
